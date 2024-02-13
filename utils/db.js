8b214988c216ad3ebf63d79c1bca304a7ef7f216
tobias-barakaa <ngairato@gmail.com>
2024-02-13 12:33:57 +0300

    Delta compression using up to 4 threads

--- utils/db.js
+++ utils/db.js
@@ -1,47 +1,27 @@
-// utils/db.js
-
+// file 'db.js'
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor() {
-        this.DB_HOST = 'localhost';
-        this.DB_PORT = 27017;
-        this.DB_DATABASE = 'files_manager';
-        this.client = new MongoClient(`mongodb://${this.DB_HOST}:${this.DB_PORT}`);
-    }
+  constructor() {
+    this.DB_HOST = 'localhost';
+    this.DB_PORT = 27017;
+    this.DB_DATABASE = 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.DB_HOST}:${this.DB_PORT}`, { useUnifiedTopology: true });
+  }
 
-    async isAlive() {
-        try {
-            await this.client.connect();
-            console.log('Connection to DB established');
-            return true;
-        } catch (error) {
-            console.error('Connection to DB failed:', error);
-            return false;
-        }
-    }
+  isAlive() {
+    this.client.connect();
+  }
 
-    async nbUsers() {
-        try {
-            const count = await this.client.db(this.DB_DATABASE).collection('users').countDocuments();
-            return count;
-        } catch (error) {
-            console.error('Error counting users:', error);
-            throw error;
-        }
-    }
+  async nbUsers() {
+    return this.client.db(this.DB_DATABASE).collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        try {
-            const count = await this.client.db(this.DB_DATABASE).collection('files').countDocuments();
-            return count;
-        } catch (error) {
-            console.error('Error counting files:', error);
-            throw error;
-        }
-    }
+  async nbFiles() {
+    return this.client.db(this.DB_DATABASE).collection('files').countDocuments();
+  }
 }
 
-const dbClient = new DBClient();
+const mongoClient = new DBClient();
 
-export default dbClient;
+export default mongoClient;
