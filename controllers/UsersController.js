64038d7bc200f9e08cb614cc2ab7622cff735fc7
tobias-barakaa <ngairato@gmail.com>
2024-02-13 16:47:39 +0300

    I can't wait

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,36 +4,25 @@ import dbClient from '../utils/db';
 
 const UsersController = {
   async postNew(req, res) {
-    try {
-      const { email, password } = req.body;
-
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
-
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-      const userExists = await dbClient.getUserByEmail(email);
-      if (userExists) {
-        return res.status(400).json({ error: 'Already exists' });
-      }
-      const hashedPassword = sha1(password);
-
-      const newUser = {
-        email,
-        password: hashedPassword,
-      };
-      const insertedUser = await dbClient.createUser(newUser);
-
-      const { _id } = insertedUser;
-      const responseUser = { id: _id, email };
-
-      return res.status(201).json(responseUser);
-    } catch (error) {
-      console.error('Error creating new user:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    // receive data from the client
+    const { email, password } = req.body;
+    // check if the email and password are provided
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+    // loop inside database and find if the email already exists
+    const user = await dbClient.usersCollection.findOne({ email });
+    // if the email already exists return an error "Already exist"
+    if (user) return res.status(400).json({ error: 'Already exist' });
+    // if the email is not found add one to the collection with password hashed
+    const result = await dbClient.usersCollection.insertOne({
+      email,
+      password: sha1(password),
+    });
+    // return the email and password hashed
+    return res.status(201).json({
+      email,
+      password: result.ops[0].password,
+    });
   },
 };
 
