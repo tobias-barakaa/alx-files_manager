fab7cb700c5450a50d30e648acc8fd43cfd6e9b0
tobias-barakaa <ngairato@gmail.com>
2024-02-13 23:59:13 +0300

    find what have been looking for

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import routes from './routes';
+// import routes from './routes';
+import controllerRouting from './routes';
+
 
 const app = express();
 const port = 5000;
 
-app.use('/', routes);
+app.use('/', controllerRouting);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
