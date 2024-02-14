30721234e6053b86cb3b8f5caaa7b5ac36434ae5
tobias-barakaa <ngairato@gmail.com>
2024-02-14 23:43:22 +0300

    what

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const allRoutes = require('./routes/index');
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRouter from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use('/', allRoutes);
+// Middleware function for logging requests
+const logMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-app.listen(port);
+// Middleware function for handling errors
+const errorHandlerMiddleware = (err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+};
+
+app.use(bodyParser.json());
+app.use(logMiddleware);
+app.use('/', userRouter);
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 module.exports = app;
+export default app;
