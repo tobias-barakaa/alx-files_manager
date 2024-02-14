41d075ed046a395de276dcf8d1df9227a254e2e4
tobias-barakaa <ngairato@gmail.com>
2024-02-14 21:30:16 +0300

    merge main

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.index);
+router.delete('/files/:id', FilesController.destroy);
+
+export default router;
