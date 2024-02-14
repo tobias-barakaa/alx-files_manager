653237be36cf9f7dda8dfc59405a4ea9d47a0f33
tobias-barakaa <ngairato@gmail.com>
2024-02-14 17:50:33 +0300

    trying

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', (req, res) => {
-  UsersController.postNew(req, res);
-});
+
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.index);
-router.delete('/files/:id', FilesController.destroy);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-module.exports = router;
 export default router;
