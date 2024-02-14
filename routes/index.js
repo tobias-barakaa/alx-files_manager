310608552b5fc9d6acd54d160a711e3b2ee799a4
tobias-barakaa <ngairato@gmail.com>
2024-02-14 18:12:09 +0300

    I do not understand what could be the problem

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,24 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/publish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
+router.get('/files', FilesController.index);
+router.delete('/files/:id', FilesController.destroy);
 
+module.exports = router;
 export default router;
