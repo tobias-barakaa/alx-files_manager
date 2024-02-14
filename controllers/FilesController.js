127d223241489871de1a9c40ea6776984a8633eb
tobias-barakaa <ngairato@gmail.com>
2024-02-15 00:35:36 +0300

    objects

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -6,99 +6,96 @@ import path from 'path';
 import dbClient from '../utils/db';
 
 const FilesController = {
-  async postUpload(req, res) {
-    const { userId } = req;
-    const { name } = req.body;
-    const { type } = req.body;
-    const { parentId } = req.body;
-    const { isPublic } = req.body;
-    const { data } = req.body;
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    if (!name) {
-      return res.status(400).json({ error: 'Missing name' });
-    }
-
-    if (!type) {
-      return res.status(400).json({ error: 'Missing type' });
-    }
-
-    if (!data) {
-      return res.status(400).json({ error: 'Missing data' });
-    }
-
-    if (type !== 'folder' && type !== 'file') {
-      return res.status(400).json({ error: 'Missing type' });
-    }
-
-    if (parentId) {
-      const parent = await dbClient.filesCollection.findOne({ _id: ObjectId(parentId) });
-
-      if (!parent) {
-        return res.status(400).json({ error: 'Parent not found' });
-      }
-
-      if (parent.type !== 'folder') {
-        return res.status(400).json({ error: 'Parent is not a folder' });
-      }
-    }
-
-    if (type === 'file') {
-      const buff = Buffer.from(data, 'base64');
-      const filePath = path.join(process.env.FOLDER_PATH, uuidv4());
-      await writeFileSync(filePath, buff);
-    }
-
-    const doc = {
-      userId,
-      name,
-      type,
-      isPublic: isPublic || false,
-      parentId: parentId || 0,
-    };
-
-    const result = await dbClient.filesCollection.insertOne(doc);
-
-    return res.status(201).json({
-      id: result.insertedId,
-      userId,
-      name,
-      type,
-      isPublic: isPublic || false,
-      parentId: parentId || 0,
-    });
-  },
-
-  async getShow(req, res) {
-    const { userId } = req;
-    const { id } = req.params;
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const file = await dbClient.filesCollection.findOne({ _id: ObjectId(id) });
-
-    if (!file) {
-      return res.status(404).json({ error: 'Not found' });
-    }
-
-    if (file.userId.toString() !== userId) {
-      return res.status(404).json({ error: 'Not found' });
-    }
-
-    return res.status(200).json({
-      id: file._id,
-      userId: file.userId,
-      name: file.name,
-      type: file.type,
-      isPublic: file.isPublic,
-      parentId: file.parentId,
-    });
-  },
+    postUpload: async (req, res) => {
+        const { name, type, data } = req.body;
+        if (!name) {
+            return res.status(400).json({ error: 'Missing name' });
+        }
+        if (!type) {
+            return res.status(400).json({ error: 'Missing type' });
+        }
+        if (!data) {
+            return res.status(400).json({ error: 'Missing data' });
+        }
+        const file = {
+            name,
+            type,
+            parentId: 0,
+        };
+        const result = await dbClient.db.collection('files').insertOne(file);
+        const { _id } = result.ops[0];
+        const filePath = path.join(__dirname, '../', process.env.FOLDER_PATH, _id);
+        writeFileSync(filePath, data, 'base64');
+        return res.status(201).json({ id: _id, name, type });
+    },
+    getShow: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        const filePath = path.join(__dirname, '../', process.env.FOLDER_PATH, fileId);
+        return res.status(200).sendFile(filePath);
+    },
+    getIndex: async (req, res) => {
+        const parentId = req.params.id || 0;
+        let files;
+        try {
+            files = await dbClient.db.collection('files').find({ parentId }).toArray();
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        return res.status(200).json(files);
+    },
+    putPublish: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        await dbClient.db.collection('files').updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: true } });
+        return res.status(200).json({ id: file._id, name: file.name, isPublic: true });
+    },
+    putUnpublish: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        await dbClient.db.collection('files').updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: false } });
+        return res.status(200).json({ id: file._id, name: file.name, isPublic: false });
+    },
+    getFile: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        const filePath = path.join(__dirname, '../', process.env.FOLDER_PATH, fileId);
+        return res.status(200).sendFile(filePath);
+    },
 };
 
 export default FilesController;
+
+
