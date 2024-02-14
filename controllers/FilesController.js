bd118425ba0d2bb266628035877a15312b614693
tobias-barakaa <ngairato@gmail.com>
2024-02-15 02:24:07 +0300

    getfile

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,107 +1,96 @@
-// controllers/FilesController.js
-
+/* eslint-disable */
 import { ObjectId } from 'mongodb';
-import fs from 'fs';
+import { writeFileSync } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
-import mime from 'mime-types';
-import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import Bull from 'bull';
-
-const fileQueue = new Bull('fileQueue');
 
 const FilesController = {
-    async postUpload(req, res) {
-        const { id } = req.userId;
-        const {
-        name, type, parentId, isPublic, data,
-        } = req.body;
-    
+    postUpload: async (req, res) => {
+        const { name, type, data } = req.body;
         if (!name) {
-        return res.status(400).json({ error: 'Missing name' });
+            return res.status(400).json({ error: 'Missing name' });
         }
-    
         if (!type) {
-        return res.status(400).json({ error: 'Missing type' });
+            return res.status(400).json({ error: 'Missing type' });
         }
-    
-        const validTypes = ['folder', 'file', 'image'];
-        if (!validTypes.includes(type)) {
-        return res.status(400).json({ error: 'Invalid type' });
+        if (!data) {
+            return res.status(400).json({ error: 'Missing data' });
         }
-    
-        if (parentId && !ObjectId.isValid(parentId)) {
-        return res.status(400).json({ error: 'parentId is not a valid ObjectID' });
-        }
-    
-        if (isPublic && typeof isPublic !== 'boolean') {
-        return res.status(400).json({ error: 'isPublic is not a boolean' });
-        }
-    
-        if (type !== 'folder' && !data) {
-        return res.status(400).json({ error: 'Missing data' });
-        }
-    
-        if (type === 'folder') {
-        const newFolder = {
-            userId: id,
+        const file = {
             name,
             type,
-            parentId: parentId || null,
-            isPublic: isPublic || false,
+            parentId: 0,
         };
-        const result = await dbClient.files.insertOne(newFolder);
-        return res.status(201).json({
-            id: result.insertedId,
-            userId: id,
-            name,
-            type,
-            parentId: parentId || null,
-            isPublic: isPublic || false,
-        });
-        }
+        const result = await dbClient.db.collection('files').insertOne(file);
+        const { _id } = result.ops[0];
+        const filePath = path.join(__dirname, '../', process.env.FOLDER_PATH, _id);
+        writeFileSync(filePath, data, 'base64');
+        return res.status(201).json({ id: _id, name, type });
+    },
+    async getShow(request, response) {
+        const fileId = request.params.id;
     
-        const buff = Buffer.from(data, 'base64');
-        const filePath = path.join(__dirname, '../../tmp', `${sha1(buff)}.${mime.extension(type)}`);
-        await fs.promises.writeFile(filePath, buff);
+        const { userId } = await userUtils.getUserIdAndKey(request);
     
-        const newFile = {
-        userId: id,
-        name,
-        type,
-        parentId: parentId || null,
-        isPublic: isPublic || false,
-        localPath: filePath,
-        };
-        const result = await dbClient.files.insertOne(newFile);
-        fileQueue.add({ userId: id, fileId: result.insertedId });
-    
-        return res.status(201).json({
-        id: result.insertedId,
-        userId: id,
-        name,
-        type,
-        parentId: parentId || null,
-        isPublic: isPublic || false,
+        const user = await userUtils.getUser({
+          _id: ObjectId(userId),
         });
     },
-    
-    async getShow(req, res) {
-        const { id } = req.userId;
-        const { fileId } = req.params;
-    
-        if (!ObjectId.isValid(fileId)) {
-        return res.status(404).json({ error: 'Not found' });
+    getIndex: async (req, res) => {
+        const parentId = req.params.id || 0;
+        let files;
+        try {
+            files = await dbClient.db.collection('files').find({ parentId }).toArray();
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
         }
-    
-        const file = await dbClient.files.findOne({ _id: ObjectId(fileId), userId: ObjectId(id) });
-    
+        return res.status(200).json(files);
+    },
+    putPublish: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
         if (!file) {
-        return res.status(404).json({ error: 'Not found' });
+            return res.status(404).json({ error: 'Not found' });
         }
-    
-        return res.status(200).json(file);
-    }
+        await dbClient.db.collection('files').updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: true } });
+        return res.status(200).json({ id: file._id, name: file.name, isPublic: true });
+    },
+    putUnpublish: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        await dbClient.db.collection('files').updateOne({ _id: ObjectId(fileId) }, { $set: { isPublic: false } });
+        return res.status(200).json({ id: file._id, name: file.name, isPublic: false });
+    },
+    getFile: async (req, res) => {
+        const fileId = req.params.id;
+        let file;
+        try {
+            file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        if (!file) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+        const filePath = path.join(__dirname, '../', process.env.FOLDER_PATH, fileId);
+        return res.status(200).sendFile(filePath);
+    },
 };
 
 export default FilesController;
+
+
